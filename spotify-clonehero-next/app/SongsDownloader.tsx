68dc2ae779d60bbd8a4a0d639621262fd89d7ead
Eli White <>
2023-10-28 17:09:18 -0700

    Function to sort song.ini file. Still need to write it to disk

--- spotify-clonehero-next/app/SongsDownloader.tsx
+++ spotify-clonehero-next/app/SongsDownloader.tsx
@@ -1,11 +1,73 @@
 'use client';
 
-import {useCallback, useState} from 'react';
+import {useCallback} from 'react';
 import {SngStream} from 'parse-sng';
+import ini from 'ini';
 
 // const URL =
 // 'https://www.enchor.us/download?md5=0acb9bad1d27efe83af51587bd20de0a&isSng=true';
 const URL = 'https://files.enchor.us/0acb9bad1d27efe83af51587bd20de0a.sng';
+
+const songIniOrder = [
+  'name',
+  'artist',
+  'album',
+  'genre',
+  'year',
+  'charter',
+  'song_length',
+  'diff_band',
+  'diff_guitar',
+  'diff_guitar_coop',
+  'diff_rhythm',
+  'diff_bass',
+  'diff_drums',
+  'diff_drums_real',
+  'diff_keys',
+  'diff_guitarghl',
+  'diff_bassghl',
+  'diff_guitarghl',
+  'diff_guitar_coop_ghl',
+  'diff_rhythm_ghl',
+  'diff_bassghl',
+  'preview_start_time',
+  'icon',
+  'loading_phrase',
+  'album_track',
+  'playlist_track',
+  'modchart',
+  'video_start_time',
+  'five_lane_drums',
+  'pro_drums',
+];
+function createSongIniString(metadata: {[key: string]: string}): string {
+  const metadataMap = new Map(Object.entries(metadata));
+
+  const knownIniValues = songIniOrder
+    .map(key => {
+      if (!metadataMap.has(key)) {
+        return;
+      }
+
+      const line = `${key} = ${metadataMap.get(key)}`;
+      metadataMap.delete(key);
+      return line;
+    })
+    // Remove empty lines missing in metadata
+    .filter(Boolean)
+    .join('\n');
+
+  const remainingValues = Array.from(metadataMap)
+    .toSorted((a, b) => {
+      return a[0].localeCompare(b[0]);
+    })
+    .map(([key, value]) => {
+      return `${key} = ${value}`;
+    })
+    .join('\n');
+
+  return `[Song]\n${knownIniValues}\n${remainingValues}`;
+}
 
 export default function SongsDownloader() {
   const handler = useCallback(async () => {
@@ -34,7 +96,6 @@ export default function SongsDownloader() {
 
     const sngStream = new SngStream((start, end) => response.body);
     // sngStream.on('file', async (file, stream) => {
-    //   console.log('file', file, stream);
     //   const fileHandle = await songDirHandle.getFileHandle(file, {
     //     create: true,
     //   });
@@ -42,19 +103,22 @@ export default function SongsDownloader() {
     //   stream.pipeTo(writableStream);
     // });
 
-    sngStream.on('files', files => {
-      console.log('files');
+    sngStream.on('header', async header => {
+      console.log('header', header);
+      const fileHandle = await songDirHandle.getFileHandle('song.ini', {
+        create: true,
+      });
+
+      createSongIniString(header.metadata);
+
+      // ini.encode({Song: header.metadata});
     });
-    sngStream.on('header', h => console.log('header', h));
 
     sngStream.on('end', () => console.log('test.sng has been fully parsed'));
 
     sngStream.on('error', error => console.log(error));
 
-    // debugger;
     sngStream.start();
-
-    // await response?.body?.pipeTo(writableStream);
   }, []);
 
   return (
