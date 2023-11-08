b53fa156a43cb8c711bdbe3ebf4db37fe1bd34c3
Eli White <>
2023-11-07 23:50:38 -0800

    Adding a page for checker

--- spotify-clonehero-next/app/checker/page.tsx
+++ spotify-clonehero-next/app/checker/page.tsx
@@ -1,11 +1,10 @@
 import SongsPicker from './SongsPicker';
 import SongsDownloader from './SongsDownloader';
 
-export default function Home() {
+export default function Checker() {
   return (
     <main className="flex max-h-screen flex-col items-center justify-between p-24">
-      <SongsPicker />
-      <SongsDownloader />
+      Checker
     </main>
   );
 }
