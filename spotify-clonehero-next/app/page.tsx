989a4b75e83ca60ff5c49079b31ebb79e47753b7
Eli White <github@eli-white.com>
2023-12-28 15:48:39 -0800

    Write some intro text

--- spotify-clonehero-next/app/page.tsx
+++ spotify-clonehero-next/app/page.tsx
@@ -1,5 +1,34 @@
-import SongsPicker from './SongsPicker';
-
 export default function Home() {
   return <div>Clone Hero Chart Tools</div>;
 }
+
+/*
+
+# Overall Intro
+This site primarily provides a collection of tools to help
+you find charts to songs you know but might not find in
+Chorus's 10s of thousands of charts.
+
+The tools on this website don't require any downloads or custom
+applications on your computer. Manage your Songs directory directly
+from your browser!
+
+Note: This website will not work on your browser. It requires some APIs that
+currently only exist in Chrome based browsers.
+
+# Tools:
+## Updates
+Check Chorus for newer/better charts than you have installed.
+Looks for newer charts that charter has published, and higher quality charts (more instruments, difficulties, and more)
+
+## Spotify Library
+Find charts on Chorus that match songs you have saved in your Spotify playlists
+
+## Spotify History (advanced!)
+If you've downloaded your Complete Listening History from your Spotify Account settings,
+this will find charts to songs you've ever listened to.
+
+## Chart Error Checker
+Primarily for charters. Check your charts for quality issues that will get flagged
+by the Chorus bot before submitting.
+*/
