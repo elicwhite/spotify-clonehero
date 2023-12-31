28be2d011c250452f314db01a8cf7590e13334f8
Eli White <github@eli-white.com>
2023-12-31 12:50:47 -0600

    Pull out chartSelection functions to make it customizable

--- spotify-clonehero-next/app/chartSelection.tsx
+++ spotify-clonehero-next/app/chartSelection.tsx
@@ -16,109 +16,32 @@ export type ChartInfo = {
   file: string; // Not sent by Encore, calculated locally
 };
 
+// If a chart is better in an earlier group, skip checking the other groups
+// If no chart is better in the first group, check the second group, and so on
+const DEFAULT_RANKING_GROUPS = [
+  [
+    testSameCharter,
+    testPreferHarmonix,
+    testPreferOfficialTracks,
+    testPreferDrums,
+  ],
+  [testPreferGuitar],
+  [testPreferHigherDiffSum],
+];
+
 export function selectChart<T extends ChartInfo>(
   charts: T[],
+  rankingGroups = DEFAULT_RANKING_GROUPS,
 ): {
   chart: T;
   reasons: string[];
 } {
-  type ChartTest = (recommendedChart: T, chart: T) => string | undefined;
-
   if (charts.length == 1) {
     return {
       chart: charts[0],
       reasons: [],
     };
   }
-
-  const testSameCharter: ChartTest = (recommendedChart: T, chart: T) => {
-    if (chart == null || recommendedChart == null) {
-      debugger;
-    }
-    if (
-      chart.charter == recommendedChart.charter &&
-      new Date(chart.modifiedTime).getTime() -
-        new Date(recommendedChart.modifiedTime).getTime() >
-        1000 // Need this check because precision of local scans is lower than Encore's APIs
-    ) {
-      return 'Chart from same charter is newer';
-    }
-  };
-
-  const testPreferHarmonix: ChartTest = (recommendedChart: T, chart: T) => {
-    if (recommendedChart.charter != 'Harmonix' && chart.charter == 'Harmonix') {
-      return 'Better chart is from Harmonix';
-    }
-  };
-
-  const testPreferOfficialTracks: ChartTest = (
-    recommendedChart: T,
-    chart: T,
-  ) => {
-    if (
-      !['Harmonix', 'Neversoft'].includes(recommendedChart.charter) &&
-      ['Harmonix', 'Neversoft'].includes(chart.charter)
-    ) {
-      return 'Better chart is from official game';
-    }
-  };
-
-  const testPreferDrums: ChartTest = (recommendedChart: T, chart: T) => {
-    if (
-      (recommendedChart.diff_drums == null ||
-        recommendedChart.diff_drums < 0) &&
-      chart.diff_drums != null &&
-      chart.diff_drums > 0
-    ) {
-      return "Better chart has drums, current chart doesn't";
-    }
-  };
-
-  const testPreferGuitar: ChartTest = (recommendedChart: T, chart: T) => {
-    if (
-      (recommendedChart.diff_guitar == null ||
-        recommendedChart.diff_guitar < 0) &&
-      chart.diff_guitar != null &&
-      chart.diff_guitar > 0
-    ) {
-      return "Better chart has guitar, current chart doesn't";
-    }
-  };
-
-  const testPreferHigherDiffSum: ChartTest = (
-    recommendedChart: T,
-    chart: T,
-  ) => {
-    // Prefer charts with higher diff_ sum
-    const recommendedDiffSum = Object.keys(recommendedChart)
-      .filter(key => key.startsWith('diff_'))
-      .map(key => recommendedChart[key as keyof T] as number)
-      .filter((value: number) => value > 0)
-      .reduce((a: number, b: number) => a + b, 0);
-
-    const chartDiffSum = Object.keys(chart)
-      .filter(key => key.startsWith('diff_'))
-      .map(key => chart[key as keyof T] as number)
-      .filter((value: number) => value > 0)
-      .reduce((a: number, b: number) => a + b, 0);
-
-    if (chartDiffSum > recommendedDiffSum) {
-      return 'Better chart has more instruments or difficulty';
-    }
-  };
-
-  // If a chart is better in an earlier group, skip checking the other groups
-  // If no chart is better in the first group, check the second group, and so on
-  const rankingGroups = [
-    [
-      testSameCharter,
-      testPreferHarmonix,
-      testPreferOfficialTracks,
-      testPreferDrums,
-    ],
-    [testPreferGuitar],
-    [testPreferHigherDiffSum],
-  ];
 
   let chartsInConsideration = charts.slice(1);
 
@@ -177,4 +100,91 @@ export function selectChart<T extends ChartInfo>(
     chart: charts[0],
     reasons: [],
   };
+}
+
+function testSameCharter<T extends ChartInfo>(
+  recommendedChart: T,
+  chart: T,
+): string | undefined {
+  if (chart == null || recommendedChart == null) {
+    debugger;
+  }
+  if (
+    chart.charter == recommendedChart.charter &&
+    new Date(chart.modifiedTime).getTime() -
+      new Date(recommendedChart.modifiedTime).getTime() >
+      1000 // Need this check because precision of local scans is lower than Encore's APIs
+  ) {
+    return 'Chart from same charter is newer';
+  }
+}
+
+function testPreferHarmonix<T extends ChartInfo>(
+  recommendedChart: T,
+  chart: T,
+): string | undefined {
+  if (recommendedChart.charter != 'Harmonix' && chart.charter == 'Harmonix') {
+    return 'Better chart is from Harmonix';
+  }
+}
+
+function testPreferOfficialTracks<T extends ChartInfo>(
+  recommendedChart: T,
+  chart: T,
+): string | undefined {
+  if (
+    !['Harmonix', 'Neversoft'].includes(recommendedChart.charter) &&
+    ['Harmonix', 'Neversoft'].includes(chart.charter)
+  ) {
+    return 'Better chart is from official game';
+  }
+}
+
+function testPreferDrums<T extends ChartInfo>(
+  recommendedChart: T,
+  chart: T,
+): string | undefined {
+  if (
+    (recommendedChart.diff_drums == null || recommendedChart.diff_drums < 0) &&
+    chart.diff_drums != null &&
+    chart.diff_drums > 0
+  ) {
+    return "Better chart has drums, current chart doesn't";
+  }
+}
+
+function testPreferGuitar<T extends ChartInfo>(
+  recommendedChart: T,
+  chart: T,
+): string | undefined {
+  if (
+    (recommendedChart.diff_guitar == null ||
+      recommendedChart.diff_guitar < 0) &&
+    chart.diff_guitar != null &&
+    chart.diff_guitar > 0
+  ) {
+    return "Better chart has guitar, current chart doesn't";
+  }
+}
+
+function testPreferHigherDiffSum<T extends ChartInfo>(
+  recommendedChart: T,
+  chart: T,
+): string | undefined {
+  // Prefer charts with higher diff_ sum
+  const recommendedDiffSum = Object.keys(recommendedChart)
+    .filter(key => key.startsWith('diff_'))
+    .map(key => recommendedChart[key as keyof T] as number)
+    .filter((value: number) => value > 0)
+    .reduce((a: number, b: number) => a + b, 0);
+
+  const chartDiffSum = Object.keys(chart)
+    .filter(key => key.startsWith('diff_'))
+    .map(key => chart[key as keyof T] as number)
+    .filter((value: number) => value > 0)
+    .reduce((a: number, b: number) => a + b, 0);
+
+  if (chartDiffSum > recommendedDiffSum) {
+    return 'Better chart has more instruments or difficulty';
+  }
 }
