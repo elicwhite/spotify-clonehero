80f50ed3c88262d2e318c7fc4faf1a5d88dcb42a
Eli White <github@eli-white.com>
2023-12-30 21:37:39 -0600

    Update the top of the Updater tool, and add more TODO items

--- spotify-clonehero-next/app/updates/SongsPicker.tsx
+++ spotify-clonehero-next/app/updates/SongsPicker.tsx
@@ -61,6 +61,9 @@ function songsReducer(state: SongState, action: SongStateActions): SongState {
   }
 }
 
+/* TODO:
+- Show progress indicator while downloading db from Enchor
+*/
 export default function SongsPicker() {
   const [songsState, songsDispatch] = useReducer(songsReducer, {
     songs: null,
@@ -162,10 +165,13 @@ export default function SongsPicker() {
   return (
     <>
       <p className="mb-4 text-center">
-        This tool is in-progress.
+        This tool checks your installed charts for updates,
         <br />
-        The goal is to be a browser based chart library manager, <br />
-        enabling you to easily update charts with new versions
+        as well as better charts for those songs.
+        <br />
+        This tool is currently in beta, it is recommended that you
+        <br />
+        backup your Songs folder before using this tool.
       </p>
 
       <Button
