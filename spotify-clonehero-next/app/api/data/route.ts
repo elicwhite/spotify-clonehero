228e640613b710a15cdcc4e9bb28d195790a2d22
Eli White <github@eli-white.com>
2025-02-09 16:50:40 -0800

    Bump the data version

--- spotify-clonehero-next/app/api/data/route.ts
+++ spotify-clonehero-next/app/api/data/route.ts
@@ -1,7 +1,7 @@
 export async function GET(request: Request) {
   return Response.json({
     // Increment this if you want to force clients to redownload server data
-    chartsDataVersion: 4,
+    chartsDataVersion: 5,
   });
 }
 
