2cbfe4f4ab2dfe9e6831c5a2c3fdf64cf3a17e74
Eli White <github@eli-white.com>
2025-02-27 23:26:07 -0800

    Delete old preview code

--- spotify-clonehero-next/lib/preview/midi.ts
+++
@@ -1,9 +0,0 @@
-import {ChartFile} from './interfaces';
-import MIDIFile from 'midifile';
-import {MidiParser} from './midi-parser';
-
-export function parseMidi(midiFileBuffer: ArrayBuffer) /*Promise<ChartFile>*/ {
-  const midiFile = new MIDIFile(midiFileBuffer);
-  const parsed = new MidiParser(midiFile).parse();
-  return parsed;
-}
