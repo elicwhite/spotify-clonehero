e331e0ea1f1918d6ef6d6c692d6638e488f4df5e
Eli White <>
2025-08-02 00:22:39 -0700

    Make the supported browser warning not render in SSR

--- spotify-clonehero-next/app/SupportedBrowserWarning.tsx
+++ spotify-clonehero-next/app/SupportedBrowserWarning.tsx
@@ -1,17 +1,23 @@
 'use client';
 
-const DIRECTORY_PICKER_SUPPROTED =
-  typeof window !== 'undefined' &&
-  typeof window.showDirectoryPicker === 'function';
-
-const NOT_SUPPORTED = !DIRECTORY_PICKER_SUPPROTED;
+import { useState, useEffect } from "react";
 
 export default function SupportedBrowserWarning({
   children,
 }: {
   children?: React.ReactNode;
 }) {
-  if (NOT_SUPPORTED) {
+  const [isSupported, setIsSupported] = useState(true);
+
+  useEffect(() => {
+    const DIRECTORY_PICKER_SUPPROTED =
+      typeof window !== 'undefined' &&
+      typeof window.showDirectoryPicker === 'function';
+
+    setIsSupported(DIRECTORY_PICKER_SUPPROTED);
+  }, [])
+
+  if (!isSupported) {
     return (
       <p className="text-lg text-red-700 mt-2">
         Warning: These tools will not work on your browser. It requires some
