70501e058d80c7e3a6c41a6b797157ce69d92867
Eli White <github@eli-white.com>
2025-09-01 00:50:29 -0700

    Format database types

--- spotify-clonehero-next/@types/database.types.ts
+++ spotify-clonehero-next/@types/database.types.ts
@@ -3,280 +3,282 @@ export type Json =
   | number
   | boolean
   | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+  | {[key: string]: Json | undefined}
+  | Json[];
 
 export type Database = {
   // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
-    PostgrestVersion: "13.0.4"
-  }
+    PostgrestVersion: '13.0.4';
+  };
   public: {
     Tables: {
       enchor_songs: {
         Row: {
-          artist: string
-          charter: string | null
-          hash: string
-          id: string
-          name: string
-        }
+          artist: string;
+          charter: string | null;
+          hash: string;
+          id: string;
+          name: string;
+        };
         Insert: {
-          artist: string
-          charter?: string | null
-          hash: string
-          id?: string
-          name: string
-        }
+          artist: string;
+          charter?: string | null;
+          hash: string;
+          id?: string;
+          name: string;
+        };
         Update: {
-          artist?: string
-          charter?: string | null
-          hash?: string
-          id?: string
-          name?: string
-        }
-        Relationships: []
-      }
+          artist?: string;
+          charter?: string | null;
+          hash?: string;
+          id?: string;
+          name?: string;
+        };
+        Relationships: [];
+      };
       spotify_tokens: {
         Row: {
-          access_token: string
-          created_at: string
-          expires_at: string
-          refresh_token: string
-          updated_at: string
-          user_id: string
-        }
+          access_token: string;
+          created_at: string;
+          expires_at: string;
+          refresh_token: string;
+          updated_at: string;
+          user_id: string;
+        };
         Insert: {
-          access_token: string
-          created_at?: string
-          expires_at: string
-          refresh_token: string
-          updated_at?: string
-          user_id: string
-        }
+          access_token: string;
+          created_at?: string;
+          expires_at: string;
+          refresh_token: string;
+          updated_at?: string;
+          user_id: string;
+        };
         Update: {
-          access_token?: string
-          created_at?: string
-          expires_at?: string
-          refresh_token?: string
-          updated_at?: string
-          user_id?: string
-        }
-        Relationships: []
-      }
+          access_token?: string;
+          created_at?: string;
+          expires_at?: string;
+          refresh_token?: string;
+          updated_at?: string;
+          user_id?: string;
+        };
+        Relationships: [];
+      };
       user_saved_song_spans: {
         Row: {
-          created_at: string | null
-          difficulty: string | null
-          end_tick: number | null
-          id: string
-          song_hash: string | null
-          start_tick: number | null
-          updated_at: string | null
-          user_id: string | null
-        }
+          created_at: string | null;
+          difficulty: string | null;
+          end_tick: number | null;
+          id: string;
+          song_hash: string | null;
+          start_tick: number | null;
+          updated_at: string | null;
+          user_id: string | null;
+        };
         Insert: {
-          created_at?: string | null
-          difficulty?: string | null
-          end_tick?: number | null
-          id?: string
-          song_hash?: string | null
-          start_tick?: number | null
-          updated_at?: string | null
-          user_id?: string | null
-        }
+          created_at?: string | null;
+          difficulty?: string | null;
+          end_tick?: number | null;
+          id?: string;
+          song_hash?: string | null;
+          start_tick?: number | null;
+          updated_at?: string | null;
+          user_id?: string | null;
+        };
         Update: {
-          created_at?: string | null
-          difficulty?: string | null
-          end_tick?: number | null
-          id?: string
-          song_hash?: string | null
-          start_tick?: number | null
-          updated_at?: string | null
-          user_id?: string | null
-        }
+          created_at?: string | null;
+          difficulty?: string | null;
+          end_tick?: number | null;
+          id?: string;
+          song_hash?: string | null;
+          start_tick?: number | null;
+          updated_at?: string | null;
+          user_id?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "user_saved_song_spans_song_hash_fkey"
-            columns: ["song_hash"]
-            isOneToOne: false
-            referencedRelation: "enchor_songs"
-            referencedColumns: ["hash"]
+            foreignKeyName: 'user_saved_song_spans_song_hash_fkey';
+            columns: ['song_hash'];
+            isOneToOne: false;
+            referencedRelation: 'enchor_songs';
+            referencedColumns: ['hash'];
           },
-        ]
-      }
+        ];
+      };
       user_saved_songs: {
         Row: {
-          created_at: string | null
-          difficulty: string | null
-          id: string
-          song_hash: string | null
-          updated_at: string | null
-          user_id: string | null
-        }
+          created_at: string | null;
+          difficulty: string | null;
+          id: string;
+          song_hash: string | null;
+          updated_at: string | null;
+          user_id: string | null;
+        };
         Insert: {
-          created_at?: string | null
-          difficulty?: string | null
-          id?: string
-          song_hash?: string | null
-          updated_at?: string | null
-          user_id?: string | null
-        }
+          created_at?: string | null;
+          difficulty?: string | null;
+          id?: string;
+          song_hash?: string | null;
+          updated_at?: string | null;
+          user_id?: string | null;
+        };
         Update: {
-          created_at?: string | null
-          difficulty?: string | null
-          id?: string
-          song_hash?: string | null
-          updated_at?: string | null
-          user_id?: string | null
-        }
+          created_at?: string | null;
+          difficulty?: string | null;
+          id?: string;
+          song_hash?: string | null;
+          updated_at?: string | null;
+          user_id?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "user_saved_songs_song_hash_fkey"
-            columns: ["song_hash"]
-            isOneToOne: false
-            referencedRelation: "enchor_songs"
-            referencedColumns: ["hash"]
+            foreignKeyName: 'user_saved_songs_song_hash_fkey';
+            columns: ['song_hash'];
+            isOneToOne: false;
+            referencedRelation: 'enchor_songs';
+            referencedColumns: ['hash'];
           },
-        ]
-      }
-    }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
+type DatabaseWithoutInternals = Omit<Database, '__InternalSupabase'>;
 
-type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
+type DefaultSchema = DatabaseWithoutInternals[Extract<
+  keyof Database,
+  'public'
+>];
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
-    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof DatabaseWithoutInternals },
+    | keyof (DefaultSchema['Tables'] & DefaultSchema['Views'])
+    | {schema: keyof DatabaseWithoutInternals},
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Views'])
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
+  schema: keyof DatabaseWithoutInternals;
 }
-  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
     }
     ? R
     : never
-  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])
-    ? (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema['Tables'] &
+        DefaultSchema['Views'])
+    ? (DefaultSchema['Tables'] &
+        DefaultSchema['Views'])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof DatabaseWithoutInternals },
+    | keyof DefaultSchema['Tables']
+    | {schema: keyof DatabaseWithoutInternals},
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables']
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
+  schema: keyof DatabaseWithoutInternals;
 }
-  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
     }
     ? I
     : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+    ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof DatabaseWithoutInternals },
+    | keyof DefaultSchema['Tables']
+    | {schema: keyof DatabaseWithoutInternals},
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables']
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
+  schema: keyof DatabaseWithoutInternals;
 }
-  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
     }
     ? U
     : never
-  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+    ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
-    | keyof DefaultSchema["Enums"]
-    | { schema: keyof DatabaseWithoutInternals },
+    | keyof DefaultSchema['Enums']
+    | {schema: keyof DatabaseWithoutInternals},
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions['schema']]['Enums']
     : never = never,
 > = DefaultSchemaEnumNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
+  schema: keyof DatabaseWithoutInternals;
 }
-  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema['Enums']
+    ? DefaultSchema['Enums'][DefaultSchemaEnumNameOrOptions]
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof DatabaseWithoutInternals },
+    | keyof DefaultSchema['CompositeTypes']
+    | {schema: keyof DatabaseWithoutInternals},
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals
+  schema: keyof DatabaseWithoutInternals;
 }
-  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema['CompositeTypes']
+    ? DefaultSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
+    : never;
 
 export const Constants = {
   public: {
     Enums: {},
   },
-} as const
-Done in 3.14s.
+} as const;
