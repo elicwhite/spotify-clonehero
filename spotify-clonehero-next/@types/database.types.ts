983376ea7109dbb4b74cb3f4a86798229ed5536d
Eli White <github@eli-white.com>
2025-09-01 01:09:59 -0700

    Add the delete user method to supabase types

--- spotify-clonehero-next/@types/database.types.ts
+++ spotify-clonehero-next/@types/database.types.ts
@@ -146,7 +146,10 @@ export type Database = {
       [_ in never]: never;
     };
     Functions: {
-      [_ in never]: never;
+      delete_user: {
+        Args: Record<PropertyKey, never>;
+        Returns: undefined;
+      };
     };
     Enums: {
       [_ in never]: never;
