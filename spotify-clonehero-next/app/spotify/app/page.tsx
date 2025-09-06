6aae567e7d6b5671df55699e08081caf2e7c8b0c
Eli White <github@eli-white.com>
2025-09-06 13:55:10 -0700

    Update title metadata for spotify tool

--- spotify-clonehero-next/app/spotify/app/page.tsx
+++ spotify-clonehero-next/app/spotify/app/page.tsx
@@ -1,8 +1,11 @@
-'use client';
-
 import dynamic from 'next/dynamic';
 
 const Spotify = dynamic(() => import('./Spotify'));
+
+export const metadata = {
+  title: 'Spotfy Chart Finder',
+  description: 'Find Charts for songs in your Spotify Library',
+};
 
 export default function Page() {
   return <Spotify />;
--- spotify-clonehero-next/app/spotify/page.tsx
+++ spotify-clonehero-next/app/spotify/page.tsx
@@ -1,5 +1,10 @@
 import WelcomeCard from './WelcomeCard';
 
+export const metadata = {
+  title: 'Spotfy Chart Finder',
+  description: 'Find Charts for songs in your Spotify Library',
+};
+
 export default function Page() {
   return <WelcomeCard />;
 }
