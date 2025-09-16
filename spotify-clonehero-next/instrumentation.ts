6179b56d7bd4eff913a67210cb1b8149114869d4
Eli White <github@eli-white.com>
2025-09-15 23:46:42 -0700

    More sentry configuration

--- spotify-clonehero-next/instrumentation.ts
+++ spotify-clonehero-next/instrumentation.ts
@@ -1,17 +1,13 @@
 import * as Sentry from '@sentry/nextjs';
 
-export function register() {
-  // The config you add here will be used whenever the server handles a request.
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-  Sentry.init({
-    dsn: 'https://ef4de5241935af48ae2c81fbc23c6a46@o4506522084048896.ingest.sentry.io/4506522086080512',
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config');
+  }
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: true,
-  });
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
+  }
 }
 
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
+export const onRequestError = Sentry.captureRequestError;
