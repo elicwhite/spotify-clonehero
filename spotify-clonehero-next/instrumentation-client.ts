04732b415c5d04655f8f30ece3781943aa66d502
Eli White <github@eli-white.com>
2025-09-20 10:58:51 -0700

    Change sentry replay sample rate

--- spotify-clonehero-next/instrumentation-client.ts
+++ spotify-clonehero-next/instrumentation-client.ts
@@ -16,10 +16,10 @@ Sentry.init({
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.0,
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
