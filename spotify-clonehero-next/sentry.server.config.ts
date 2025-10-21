d7516fc3cd0c2927f4b1335a19dfefd9a508a3e4
Eli White <github@eli-white.com>
2025-10-21 07:38:53 -0700

    Don't log to sentry in DEV

--- spotify-clonehero-next/sentry.server.config.ts
+++ spotify-clonehero-next/sentry.server.config.ts
@@ -2,14 +2,16 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://ef4de5241935af48ae2c81fbc23c6a46@o4506522084048896.ingest.us.sentry.io/4506522086080512",
+  dsn: 'https://ef4de5241935af48ae2c81fbc23c6a46@o4506522084048896.ingest.us.sentry.io/4506522086080512',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  enabled: process.env.NODE_ENV !== 'development',
 });
