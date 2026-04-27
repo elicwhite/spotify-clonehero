f4ee5238217603e10bc09cfafc6a5c253843510a
Eli White <github@eli-white.com>
2026-04-26 17:20:50 -0700

    Delete sheet-music-spotify page

--- spotify-clonehero-next/app/sheet-music-spotify/page.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-
-// const SheetMusicSpotify = dynamic(() => import('./SheetMusicSpotify'));
-const SpotifyLibrary = dynamic(() => import('./SpotifyLibrary'));
-
-export default function Page() {
-  return <SpotifyLibrary />;
-}
